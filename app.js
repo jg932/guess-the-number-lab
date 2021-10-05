709f7b9f7cd378083d4f9582d16d859a99f1f8d2
jg932 <jared.h.goldstein@gmail.com>
2021-10-04 20:15:54 -0400

    bonus included

--- app.js
+++ app.js
@@ -21,17 +21,32 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-    this.currentGuess = game.getGuess();
     while(this.currentGuess !== this.secretNum){
-      game.prevGuesses.push(this.currentGuess);
-      game.render()
-      // console.log("\n\n\n\n\n\n\n\n" + this.currentGuess, this.secretNum)
+      if (this.currentGuess !== null){
+        game.render();
+        game.update();
+      }
+      console.log("\n\n\n\n\n\n\n\n" + this.currentGuess, this.secretNum)
       this.currentGuess = game.getGuess()
     }
     window.alert(`You win! You got it in ${this.prevGuesses.length} guesses!`);
   },
   getGuess: function(){
-    return parseInt(window.prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}!`));
+    let guess = parseInt(window.prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}!`));
+    console.log ("\n\n\n\n\n\n" + guess)
+    if (guess < game.smallestNum){
+      window.alert(`Invalid entry, guess must be more than ${game.smallestNum}`)
+      guess = null
+    } else if (guess > game.biggestNum){
+      window.alert(`Invalid entry, guess must be less than ${game.biggestNum}`)
+      guess = null
+    } else if (isNaN(guess)){
+      window.alert(`Numbers only smart ass!`)
+      guess = null
+    } else {
+      game.prevGuesses.push(guess);
+    }
+    return guess;
   },
   render: function(){
     const prettyGuesses = this.prevGuesses.join(', ');
@@ -43,15 +58,14 @@ const game = {
   }, 
   update: function(){
     if(this.currentGuess > this.secretNum){
-      this.currentGuess = biggestNum
-    } else {
-    if(this.currentGuess < this.secretNum){
-      this.currentGuess = smallestNum
-    }
+      this.biggestNum = this.currentGuess
+    } else if (this.currentGuess < this.secretNum){
+      this.smallestNum = this.currentGuess
     }
   },
 }
 
 
 game.play()
+
 
